d7b5ec1fc92ce4a7050f6ee07996cf2d994d15fe
halo-magical <jjgwlsry@mz.co.kr>
2026-07-10 10:04:47 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-systemctl restart httpd
-systemctl enable httpd
+if systemctl list-unit-files | grep -q httpd.service; then
+    systemctl restart httpd
+    systemctl enable httpd
+else
+    systemctl restart apache2
+    systemctl enable apache2
+fi
